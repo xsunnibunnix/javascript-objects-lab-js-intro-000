24c297ac74edacc93f4552f7464ed1024cf1a7a6
Sunny Pacheco <pacheco.sunny@gmail.com>
2021-02-11 00:50:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 var recipe = {
   eggs: 5,
   flour: 2 cups,
-  milk: 1 cup,
-}
-function updateObjectWithKeyAndValue(recipe, key, value) {
-  return Object.assign({}, recipe, {[key]: value})
+  milk: 1 cup
 }
